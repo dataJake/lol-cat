298487937275b615957630934cdf6e748662e21e
Jake <j.a.mehrmann@gmail.com>
2020-04-20 17:35:54 -0400

    Got clock updating in real time

---
+++ main.js
@@ -0,0 +1,40 @@
+var currentTime;
+var flyTime;
+var stretchTime;
+var dinnerTime;
+var gymTime;
+var partyTime;
+var morning = 7;
+var evening = 18;
+var noon = 12;
+var sleepyTime = 22;
+
+var displayCurrentTime = function() {
+
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var amOrPm = "am";
+    var time;
+    var clock = document.getElementById('clock');
+
+    if (hours >= 12) {
+        amOrPm = "pm";
+        hours = hours - 12;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    time = hours + ":" + minutes + ":" + seconds + " " + amOrPm;
+
+    clock.innerHTML = time;
+}
+
+setInterval(displayCurrentTime, 1000);
