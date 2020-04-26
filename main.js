ad652debc7f212fa68f0fb31cb008e93d56c77be
Jake <j.a.mehrmann@gmail.com>
2020-04-26 14:26:30 -0400

    Wrote party button script and resized all images to fit

--- main.js
+++ main.js
@@ -37,4 +37,69 @@ var displayCurrentTime = function() {
     clock.innerHTML = time;
 }
 
-setInterval(displayCurrentTime, 1000);
+var picChanger = function() {
+
+    var picTime = new Date().getHours();
+    var caption;
+    var banner = document.getElementById("main-image");
+
+    if (picTime === partyTime) {
+        caption = "Let's party";
+        banner.style.backgroundImage = "url('assets/party-time.jpg')"
+    }
+    else if (picTime === flyTime) {
+        caption = "Time to fly!";
+        banner.style.backgroundImage = "url('assets/gotta-fly.jpg')";
+    }
+    else if (picTime === stretchTime) {
+        caption = "Stretch it out!";
+        banner.style.backgroundImage = "url('/assets/part-time.jpg')";
+    }
+    else if (picTime === dinnerTime) {
+        caption = "This iz not cheezburger : (";
+        banner.style.backgroundImage = "url('assets/tomato_cat.jpg')";
+    }
+    else if (picTime === gymTime) {
+        caption = "Let's get swole!";
+        banner.style.backgroundImage = "url('assets/time-to-hit-the-gym.jpg')";
+    }
+    else if (picTime >= 7 || picTime < 12) {
+        caption = "Good morning!";
+        banner.style.backgroundImage = "url('assets/time-for-romance.jpg')";
+    }
+    else if (picTime < 18) {
+        caption = "Good afternoon!";
+        banner.style.backgroundImage = "url('assets/dinner-time.jpg')";
+    }
+    else if (picTime < 22) {
+        caption = "Good evening!";
+        banner.style.backgroundImage = "url('assets/happy-hour.jpg')";
+    }
+    else {
+        caption = "Sweet dreamzzz";
+        banner.style.backgroundImage = "url('assets/sleepy_kitty')"
+    }
+
+    banner.innerText = caption;
+    displayCurrentTime();
+}
+
+setInterval(picChanger, 1000);
+
+var partyButton = document.getElementById("party-button");
+
+var letsParty = function() {
+    if (partyTime < 0) {
+        partyTime = new Date().getHours();
+        partyButton.innerText = "Party Over!";
+        partyButton.style.backgroundColor = "#B0CAE5";
+    }
+    else {
+        partyTime = -1;
+        partyButton.innerText = "Party Time!";
+        partyButton.style.backgroundColor = "#E4593A";
+    }
+}
+
+partyButton.addEventListener("click", letsParty);
+letsParty();
